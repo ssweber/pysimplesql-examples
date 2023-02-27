eb9821ac19c7193ab386b7189f1fd2224d1970f9
ssweber <57631333+ssweber@users.noreply.github.com>
2023-02-26 20:26:02 -0500

    cleanup

--- tests/parent child grandchild.py
+++ tests/parent child grandchild.py
@@ -2,25 +2,22 @@
 
 ## Delete from parent, only cascades to child. Grandchildren are not deleted
 ## Duplicate from parent, only cascades to child. Grandchildren are not duplicated
-
-grandchild = True  ### set this to False to only be parent/child
-
 import PySimpleGUI as sg  ## pysimplegui 4.60.4
 import pysimplesql as ss
 import logging
-import timeit
 import time
 
-sz = (600, 250)
+tables = True # Set this to False to use sg.Combo for selectors.
+sz = (600, 250) # for layouts
+grandchild = True  # Set this to False to only be parent/child
+quick_editor = True  # quick_editor=quick_editor
+enable_id = 1 # to see ID on tables.
+_tabs_ = "-TABGROUP-"
+
 logger = logging.getLogger(__name__)
 logging.basicConfig(
     level=logging.INFO
 )  # <=== You can set the logging level here (NOTSET,DEBUG,INFO,WARNING,ERROR,CRITICAL)
-_tabs_ = "-TABGROUP-"
-
-quick_editor = True  # quick_editor=quick_editor
-enable_id = 0
-tables = True
 
 sql_grandchild = """
 CREATE TABLE IF NOT EXISTS "bike_repair" (
@@ -80,15 +77,15 @@ CREATE TABLE IF NOT EXISTS "building" (
 	PRIMARY KEY("id" AUTOINCREMENT)
 );
 {sql_grandchild}
-INSERT INTO "car" VALUES (1,'Soul',1,1);
+INSERT INTO "car" VALUES (1,'Landrover',1,1);
 INSERT INTO "car" VALUES (2,'Jeep',1,2);
 INSERT INTO "car" VALUES (3,'GTO',1,3);
 INSERT INTO "bike" VALUES (1,'Unicycle',1,1);
-INSERT INTO "bike" VALUES (2,'Street',1,2);
-INSERT INTO "bike" VALUES (3,'Mountain',1,3);
+INSERT INTO "bike" VALUES (2,'Street Bike',1,2);
+INSERT INTO "bike" VALUES (3,'Moped',1,3);
 INSERT INTO "person" VALUES (1,'Bill',0);
 INSERT INTO "person" VALUES (2,'Jessica',0);
-INSERT INTO "person" VALUES (3,'Polly',0);
+INSERT INTO "person" VALUES (3,'Drake',0);
 INSERT INTO "building" VALUES (1,'Tower',0);
 INSERT INTO "building" VALUES (2,'Mall',0);
 INSERT INTO "building" VALUES (3,'Cabin',0);
@@ -111,7 +108,7 @@ else:
     selector = [ss.selector("_selBuilding_", "building", sg.Combo)]
 
 building_layout = [
-    [sg.Text("Childless Parent")],
+    [sg.Text("Buildings - Childless Parent")],
     selector,
     [ss.record("building.name"), ss.record("building.example", sg.Checkbox)],
     [ss.actions("_actBuilding_", "building", default=True)],
@@ -128,7 +125,7 @@ else:
     selector = [ss.selector("_selPerson_", "person", sg.Combo)]
 # Define the columns for the table selector
 person_layout = [
-    [sg.Text("Parent")],
+    [sg.Text("Person - Parent w/ cascade")],
     selector,
     [ss.record("person.name"), ss.record("person.example", sg.Checkbox)],
     [ss.actions("_actPerson_", "person", default=True)],
@@ -146,7 +143,7 @@ if tables:
 else:
     selector = [ss.selector("_selCar_", "car", sg.Combo)]
 car_layout = [
-    [sg.Text("Person Child/ Bike Sibling")],
+    [sg.Text("Car - Child of Person/ Sibling of Bike")],
     selector,
     [ss.record("car.name"), ss.record("car.example", sg.Checkbox)],
     [
@@ -165,7 +162,7 @@ if tables:
 else:
     selector = [ss.selector("_selBike_", "bike", sg.Combo)]
 bike_layout = [
-    [sg.Text("Person Child/ Car Sibling")],
+    [sg.Text("Bike - Child of Person/ Sibling of Car")],
     selector,
     [ss.record("bike.name"), ss.record("bike.example", sg.Checkbox)],
     [ss.record("bike.person_id", sg.Combo)],
@@ -183,7 +180,7 @@ else:
     selector = [ss.selector("_selRepair_", "bike_repair", sg.Combo)]
 bike_repair_layout = [
     [sg.HorizontalSeparator()],
-    [sg.Text("Bike child, Person Grandchild")],
+    [sg.Text("Bike Repair - Bike child, Person Grandchild")],
     selector,
     [ss.record("bike_repair.name"), ss.record("bike_repair.example", sg.Checkbox)],
     [ss.record("bike_repair.bike_id", sg.Combo)],
@@ -201,7 +198,7 @@ else:
     selector = [ss.selector("_selStyle_", "style", sg.Combo)]
 style_layout = [
     [sg.HorizontalSeparator()],
-    [sg.Text("BikeRepair Child, Person Grandgrandchild")],
+    [sg.Text("Repair Upgrade - Child of BikeRepair / Grandgrandchild of Person")],
     selector,
     [ss.record("style.name"), ss.record("style.example", sg.Checkbox)],
     [ss.record("style.bike_repair_id", sg.Combo)],
@@ -212,14 +209,14 @@ style_layout = [
 # Main Layout
 # -------------------------
 
-layout = [[sg.Button("Form Prompt_Save", key="save")],[sg.Button("Timeit", key="-timeit-")]]
+layout = [[sg.Button("Form Prompt_Save", key="save")],[sg.Button("50 selector switch test", key="-timeit-")]]
 layout.append([sg.Col(person_layout, size=sz), sg.Col(building_layout, size=sz)])
 layout.append([sg.Col(bike_layout, size=sz), sg.Col(car_layout, size=sz)])
 if grandchild:
     layout.append([sg.Col(bike_repair_layout, size=sz), sg.Col(style_layout, size=sz)])
 
 window = sg.Window(
-    "People and Vehicals",
+    "People and Vehicles",
     layout,
     finalize=True,
     grab_anywhere=True,
