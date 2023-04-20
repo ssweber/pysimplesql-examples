f5282d36c03a06ef490ecdb78dc844f6f8105155
ssweber <57631333+ssweber@users.noreply.github.com>
2023-04-20 14:04:47 -0400

    added progressbar to show animation

--- self_closing_popup.py
+++ self_closing_popup.py
@@ -19,7 +19,7 @@ class Popup:
         :param auto_close_seconds: Time in seconds after which the window will automatically close.
         :return: None
         """
-        
+
         # Split the message string into lines
         msg_lines = msg.splitlines()
 
@@ -33,6 +33,7 @@ class Popup:
             keep_on_top=True,
             finalize=True,
             enable_close_attempted_event=True,
+            location=(100, 100),
         )
         self.window.TKroot.after(auto_close_seconds * 1000, self._auto_close)
 
@@ -44,16 +45,22 @@ class Popup:
 
 
 layout = [
-    [
-        sg.Button("Test", key="test"),
-    ]
+    [sg.Button("Test", key="test")],
+    [sg.ProgressBar(100, orientation="h", expand_x=True, size=(20, 20), key="-PBAR-")],
 ]
 
-window = sg.Window("Non Blocking Info Popup", layout, finalize=True, grab_anywhere=True,)
+window = sg.Window(
+    "Non Blocking Info Popup",
+    layout,
+    finalize=True,
+    grab_anywhere=True,
+    element_justification="center",
+)
 popup = Popup(window)
+pb_value = 0
 
 while True:
-    event, values = window.read()
+    event, values = window.read(timeout=100)
     if event == sg.WINDOW_CLOSED:
         break
     if event == "test":
@@ -62,4 +69,10 @@ while True:
             msg="I'll close in 5 seconds\nAnd I'm non-blocking",
             auto_close_seconds=5,
         )
+    if event == "__TIMEOUT__":
+        if pb_value < 100:
+            pb_value += 10
+        else:
+            pb_value = 0
+        window["-PBAR-"].update(current_count=pb_value)
 window.close()
